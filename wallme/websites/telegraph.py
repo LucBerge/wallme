ad8d3a53fe76e00160c4fab2183ac09400d3297e
Lucas <lucas.bergeron@outlook.fr>
2019-08-20 02:29:07 +0200

    [telegraph] Update

--- wallme/websites/telegraph.py
+++ wallme/websites/telegraph.py
@@ -1,11 +1,10 @@
 import re
 
 NAME = 'telegraph'
-DESCRIPTION = 'The Telegrapg picture of the day'
+DESCRIPTION = 'The Telegraph picture of the day'
 
 def getWebPageUrl(date):
 	month = date.strftime('%B').lower()
-	return 'https://www.telegraph.co.uk/news/2019/08/19/pictures-day-19-august-2019/'
 	return date.strftime("https://www.telegraph.co.uk/news/%Y/%m/%d/pictures-day-%d-" + month + "-%Y/")
 
 def getPictureUrl(webpage):
