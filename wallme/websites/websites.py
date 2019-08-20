c066e9602b6a601f960839b29fd2f44385ee95c0
Lucas <lucas.bergeron@outlook.fr>
2019-08-20 02:01:24 +0200

    [epod] Add support

--- wallme/websites/websites.py
+++ wallme/websites/websites.py
@@ -1,5 +1,6 @@
 import wallme.websites.apod
 import wallme.websites.bing
+import wallme.websites.epod
 import wallme.websites.nasa
 import wallme.websites.nationalgeographic
 import wallme.websites.tetras
