7a54853a4be7735e44c3b0d8b7a112fee7e386dd
Lucas Bergeron <lucas.bergeron@outlook.fr>
2022-02-10 18:29:08 +0100

    Add reddit test when no image has been found

--- test/test_reddit.py
+++ test/test_reddit.py
@@ -5,12 +5,6 @@ from wallme.websites import reddit
 
 
 class TestReddit(TestWebsite):
-    def test_reddit(self):
-        try:
-            self.simple_test("reddit", "reddit", None, reddit)
-            assert False
-        except Exception:
-            assert True
 
     def test_reddit_cityporn(self):
         self.simple_test("reddit.cityporn", "reddit", "cityporn", reddit)
@@ -20,3 +14,18 @@ class TestReddit(TestWebsite):
 
     def test_reddit_sexywomanoftheday(self):
         self.simple_test("reddit.sexywomanoftheday", "reddit", "sexywomanoftheday", reddit)
+
+    def test_reddit_error(self):
+        try:
+            self.simple_test("reddit", "reddit", None, reddit)
+            assert False
+        except Exception:
+            assert True
+
+    def test_reddit_no_image(self):
+        reddit.URL = "https://fake.url"
+        try:
+            self.simple_test("reddit.cityporn", "reddit", "cityporn", reddit)
+            assert False
+        except Exception:
+            assert True
