2d77d1631c660d3259468809f49223063c2151e8
Lucas Bergeron <lucas.bergeron@outlook.fr>
2022-11-30 16:43:33 +0100

    Exe in windowed mode

--- devscript/create_exe.sh
+++ devscript/create_exe.sh
@@ -4,6 +4,6 @@ fi
 
 rm -r dist
 echo -e "from wallme.main import main\nmain()" >> wallme.py
-pyinstaller --noconfirm --onefile --console "wallme.py"
+pyinstaller --noconfirm --onefile --windowed "wallme.py"
 rm  wallme.py wallme.spec
 rm -r build
